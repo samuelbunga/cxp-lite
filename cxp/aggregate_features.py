24e09d1fe4c0d8f45fb2315fa8e2911a95dae121
Samuel Bunga <sambunga094@gmail.com>
2021-08-04 15:54:41 -0400

    first working version of aggregate features

--- cxp/aggregate_features.py
+++ cxp/aggregate_features.py
@@ -1,12 +1,14 @@
 import re
 import sys
 import csv
+import openpyxl
 import pandas as pd
 from os import listdir
 from os.path import join
 from collections import defaultdict
 
 def aggregate_features(input_dir):
+    writer = pd.ExcelWriter(join(input_dir, 'aggregated_features.xlsx'))
     # Read the input directory for outputs
     output_dirs = [d for d in listdir(input_dir) 
                    if d.startswith('output')]
@@ -17,7 +19,9 @@ def aggregate_features(input_dir):
         features_df = pd.read_csv(join(input_dir, f, features_csv))
     # read each features file and aggregate information
     # into a dictionary
-    peak_count = {}    
+    peak_count = {}
+    amplitude = {}
+    auc = {}
     for f in output_dirs:
         features_csv = [j for j in listdir(join(input_dir, f)) 
                         if re.search(r'.*features.csv', j)][0]
@@ -25,6 +29,20 @@ def aggregate_features(input_dir):
         for r,c in features_df.iterrows() :
             if c[1] in peak_count:
                 peak_count[c[1]].update({c[0]:c[2]})
+                amplitude[c[1]].update({c[0]:c[3]})
+                auc[c[1]].update({c[0]:c[4]})
             else:
                 peak_count[c[1]] = {c[0]:c[2]}
+                amplitude[c[1]] = {c[0]:c[3]}
+                auc[c[1]] = {c[0]:c[4]}
+                
+    
+    peaks_df = pd.DataFrame(peak_count).T
+    amplitude_df = pd.DataFrame(amplitude).T
+    auc_df = pd.DataFrame(auc).T
+    
+    peaks_df.to_excel(writer, sheet_name ='Peaks', header=True, index=False)
+    amplitude_df.to_excel(writer, sheet_name ='Amplitude', header=True, index=False)
+    auc_df.to_excel(writer, sheet_name ='Auc', header=True, index=False)
+    writer.save()
 
