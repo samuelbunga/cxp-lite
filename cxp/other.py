6b12ed9493ba5327a748bd84eb96a36c4fad075d
Samuel Bunga <sambunga094@gmail.com>
2021-09-20 14:34:44 -0400

    fixed bugs and pushing the latest changes

--- cxp/other.py
+++ cxp/other.py
@@ -6,8 +6,8 @@ import sys
 
 
 def set_java_home():
-    #os.system("export JAVA_HOME=`/usr/libexec/java_home` >>~/.bash_profile")
-    #os.environ["JAVA_HOME"] = "`/usr/libexec/java_home`"
+    # os.system("export JAVA_HOME=`/usr/libexec/java_home` >>~/.bash_profile")
+    # os.environ["JAVA_HOME"] = "`/usr/libexec/java_home`"
     pass
 
 
@@ -22,15 +22,19 @@ def check_mask_file(wd):
 def _get_non_mask(wd):
     img_files = [os.path.join(wd, f) for f in os.listdir(wd)
                  if (os.path.isfile(os.path.join(wd, f))
-                     and re.search(r".*image_j.*", f))
+                     and re.search(r".*image_j.tiff", f))
                  ]
-    print(img_files)
+    img_files = [f for f in img_files
+                 if 'mask' not in f]
+
     if len(img_files) >= 1:
         outfile = open(os.path.join(wd, 'data.csv'), 'w')
         with outfile as fh:
             for i in img_files:
-                fh.write(i+'\n')
+                fh.write(i + '\n')
         return os.path.join(wd, 'data.csv')
+    else:
+        return False
 
 
 def run_cell_profiler(pipe_file, input_folder):
@@ -38,8 +42,8 @@ def run_cell_profiler(pipe_file, input_folder):
     # result = check_mask_file(os.path.dirname(pipe_file))
     run_image_j(input_folder)  # Check and run imageJ on tiff files
     data_file = _get_non_mask(input_folder)
-    #if not result:
-    if os.path.exists(data_file):
+    # if not result:
+    if os.path.exists(data_file) and data_file != False :
         os.system("/Applications/CellProfiler-3.1.9.app/Contents/MacOS/cp -c -r -p " + pipe_file + " --file-list " \
                   + data_file)
     else:
@@ -55,14 +59,14 @@ def run_image_j(wd):
 
     for i in img_files:
         input_file = os.path.basename(i.strip('\n'))
-        output_file = os.path.basename(i.strip('\n')).split('.')[0]+'_image_j.tiff'
+        output_file = os.path.basename(i.strip('\n')).split('.')[0] + '_image_j.tiff'
         os.system("java -Xmx4096m -jar /Applications/ImageJ.app/Contents/Java/ij.jar -ijpath /Applications/ImageJ.app \
         -macro " + os.path.join(os.path.dirname(__file__), 'batch.ijm ') + os.path.join(wd, input_file) + '#' + \
-        os.path.join(wd, output_file))
+                  os.path.join(wd, output_file))
         # Move original files
         os.makedirs(os.path.join(wd, 'original_images')) if not os.path.exists(os.path.join(wd, 'original_images')) \
             else False
-        os.rename(os.path.join(wd, input_file),  os.path.join(wd, 'original_images', input_file))
+        os.rename(os.path.join(wd, input_file), os.path.join(wd, 'original_images', input_file))
 
 
 if __name__ == '__main__':
